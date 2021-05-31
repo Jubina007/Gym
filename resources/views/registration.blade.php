b1f5493463acacbb08c482e4412486a74c81fe41
Jubina Biju <jubinabiju007@gmail.com>
2021-05-31 21:38:18 +0530

    k

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -3,7 +3,7 @@
 
 @section("content")
         
-<body background="https://images.pexels.com/photos/4753987/pexels-photo-4753987.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="img-thumbnail"  style="height: 100%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+<body background="https://www.mensjournal.com/wp-content/uploads/2019/04/Gym.jpg?quality=86&strip=all" class="img-thumbnail"  style="height: 100%; background-position: center; background-repeat: no-repeat; background-size: cover;">
     <div class="container">
             <div class="row">
                 <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
